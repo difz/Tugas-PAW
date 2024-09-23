681166cad318a72f22d8253b11c89b4782836d8e
Nur Rochman <nurrochman954@gmail.com>
2024-09-23 22:26:55 +0700

    edit controllers and routes

--- code/src/routes/user_routes.js
+++ code/src/routes/user_routes.js
@@ -1,9 +1,10 @@
 const UserRouter = require('express').Router();
 
-const {createUser, updateUser, deleteUser} = require('../controllers/user_control');
+const {createUser, updateUser, deleteUser, getAllUsers} = require('../controllers/user_control');
 
 UserRouter.post('/create', createUser);
 UserRouter.put('/update/:id', updateUser);
 UserRouter.delete('/delete/:id', deleteUser);
+UserRouter.get('/all', getAllUsers);
 
-module.exports = UserRouter;    
+module.exports = UserRouter;  
