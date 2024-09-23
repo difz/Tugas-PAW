cf1d5657569d79af654336bf41fdf24a924b2aab
Drafaund <faundrapratamasukma505520@mail.ugm.ac.id>
2024-09-23 16:32:30 +0700

    editing index.js and .env

--- code/index.js
+++ code/index.js
@@ -1,5 +1,16 @@
+const e = require('cors');
 const express = require('express'); 
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect();
+//MONGO DB CONNECTION
+if (!process.env.MONGO_URI) {
+    throw Error("Database connection string not found");
+  }
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Succesfully connected to MongoDB");
+    }).catch((err) => {
+      console.log("Failed to connect to MongoDB");
+      console.log(err);
+    });
